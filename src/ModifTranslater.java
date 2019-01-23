70c9fbcc7a7fa18cde6fd063c2cf572bed235536
Yaroslav Obruch <42571929+neopite@users.noreply.github.com>
2019-01-23 19:10:17 +0200

    Refactor exceptions and fix dictionary load

--- src/ModifTranslater.java
+++ src/ModifTranslater.java
@@ -7,16 +7,18 @@ public class ModifTranslater {
     final static ArrayList<ArrayList<String>> LIST = new ArrayList<>();
     final static HashMap<String, Integer> COUNT_OF_LANGUAGES = new HashMap<>();
     final static HashMap<Integer, String> LIST_OF_LANGUAGES = new HashMap<>();
+    final static HashMap<String, HashMap<String, String>> DICTIONARY_LOAD = new HashMap<>();
+    final static HashMap<String, HashMap<String, String>> DICTIONARY_LOAD_TO = new HashMap<>();
+    static String firstLanguage;
 
-    public static void main(String[] args) throws FileNotFoundException {
-        putVocabulars();
-        setAListOfLanguages();
+
+    public static void main(String[] args) {
+        putVocabularsAndDictionary();
         putNewLanguage();
-        String firstLanguage = "";
         Scanner write = new Scanner(System.in);
         System.out.println("Enter sentence: ");
         String sentence = write.nextLine();
-        firstLanguage = languageDetection(sentence, LIST_OF_LANGUAGES, COUNT_OF_LANGUAGES);
+        firstLanguage = languageDetection(sentence);
         System.out.println("Detected language : " + firstLanguage);
         System.out.println("To which Language translate");
         String secondLanguage = write.nextLine();
@@ -32,79 +34,89 @@ public class ModifTranslater {
             if (secondLanguage.equalsIgnoreCase("Eng")) {
                 translateToEnglish(secondLanguage, sentence);
                 break;
-            }else
+            } else
                 translateFromEnglish(translateToEnglish(firstLanguage, sentence), secondLanguage);
-                break;
+            break;
+        }
+    }
+
+    public static HashMap<String, String> loadDictinory(String file) {
+        HashMap<String, String> vocabular = new HashMap<>();
+        try {
+            Scanner read = new Scanner(new FileReader(file));
+            while (read.hasNextLine()) {
+                String line = read.nextLine();
+                String[] lineArray = line.split("-");
+                vocabular.put(lineArray[0], lineArray[1]);
             }
-        }
-
-    public static HashMap<String, String> loadDictinory(String file) throws FileNotFoundException {
-        HashMap<String, String> vocabular = new HashMap<>();
-        Scanner read = new Scanner(new FileReader(file));
-        while (read.hasNextLine()) {
-            String line = read.nextLine();
-            String[] lineArray = line.split("-");
-            vocabular.put(lineArray[0], lineArray[1]);
+        } catch (FileNotFoundException error) {
+            System.out.println("File(" + file + ") not found,please try again");
+            System.exit(-1);
         }
         return vocabular;
     }
 
-    public static HashMap<String, String> loadDictinoryTo(String file) throws FileNotFoundException {
+    public static HashMap<String, String> loadDictinoryTo(String file) {
         HashMap<String, String> vocabular = new HashMap<>();
-        Scanner read = new Scanner(new FileReader(file));
-        while (read.hasNextLine()) {
-            String line = read.nextLine();
-            String[] lineArray = line.split("-");
-            vocabular.put(lineArray[1], lineArray[0]);
+        try {
+            Scanner read = new Scanner(new FileReader(file));
+            while (read.hasNextLine()) {
+                String line = read.nextLine();
+                String[] lineArray = line.split("-");
+                vocabular.put(lineArray[1], lineArray[0]);
+            }
+        } catch (FileNotFoundException error) {
+            System.out.println("File(" + file + ") not found,please try again");
+            System.exit(-1);
         }
         return vocabular;
     }
 
-    public static String translateToEnglish(String lang, String sentence) throws FileNotFoundException {
-        Scanner scanner = new Scanner(System.in);
+    public static String translateToEnglish(String lang, String sentence) {
         String str = "";
         switch (lang) {
             case "Eng":
-                System.out.println("Which dictionary to use?");
-                String dict = scanner.nextLine();
                 for (String words : sentence.split(" ")) {
-                    str += loadDictinory(dict).get(words) + " ";
+                    str += DICTIONARY_LOAD.get(firstLanguage).get(words) + " ";
                 }
                 System.out.println(str);
                 break;
             default:
                 for (String words : sentence.split(" ")) {
-                    str += loadDictinory(lang).get(words) + " ";
+                    str += DICTIONARY_LOAD.get(lang).get(words) + " ";
                 }
         }
         return str;
     }
 
-    public static void translateFromEnglish(String str, String lang) throws FileNotFoundException {
-        for (String line : str.split(" ")) {
-            System.out.print(loadDictinoryTo(lang).get(line) + " ");
+    public static void translateFromEnglish(String str, String lang) {
+        try {
+            for (String line : str.split(" ")) {
+                System.out.print(DICTIONARY_LOAD_TO.get(lang).get(line) + " ");
+            }
+        }catch (NullPointerException error){
+            System.out.println("Language("+lang+")is not contained in dictionaries");
+            System.exit(-1);
         }
     }
 
-    public static ArrayList<String> listOfWords(String language) throws FileNotFoundException {
+    public static ArrayList<String> listOfWords(String language) {
         ArrayList<String> listOFword = new ArrayList<>();
-        return readFile(listOFword,language);
+            return readFile(listOFword, language);
     }
 
-    public static String languageDetection(String sentence, HashMap<Integer, String> numbersOfLanguages,
-                                           HashMap<String, Integer> list) throws FileNotFoundException {
+    public static String languageDetection(String sentence) {
         for (String word : sentence.split(" ")) {
             for (int x = 0; x < LIST.size(); x++) {
                 for (int z = 0; z < findSizeOfVocab(LIST_OF_LANGUAGES.get(x)); z++) {
                     if (word.equalsIgnoreCase((LIST.get(x).get(z)))) {
                         findLanguageForTranslation(LIST_OF_LANGUAGES, x, COUNT_OF_LANGUAGES);
-
                     }
                 }
 
             }
         }
-        String language = "";
+        String language = "De";
         int maxValueInMap = (Collections.max(COUNT_OF_LANGUAGES.values()));
         for (Map.Entry<String, Integer> entry : COUNT_OF_LANGUAGES.entrySet()) {
             if (entry.getValue() == maxValueInMap) {
@@ -116,39 +128,49 @@ public class ModifTranslater {
 
     }
 
-    public static int findSizeOfVocab(String language) throws FileNotFoundException {
+    public static int findSizeOfVocab(String language)  {
         int count = 0;
         File file = new File(language);
-        Scanner read = new Scanner(new FileReader(file));
-        while (read.hasNext()) {
-            String line = read.nextLine();
-            count++;
+        try {
+            Scanner read = new Scanner(new FileReader(file));
+            while (read.hasNext()) {
+                String line = read.nextLine();
+                count++;
+            }
+        }catch (FileNotFoundException error){
+            System.out.println("File("+file+") not found,please try again");
+            System.exit(-1);
         }
         return count;
     }
 
-    public static void putVocabulars() throws FileNotFoundException {
+    public static void putVocabularsAndDictionary()  {
         final ArrayList<String> ENG = listOfWords("Eng");
         final ArrayList<String> RUS = listOfWords("Rus");
         final ArrayList<String> UA = listOfWords("Ua");
+        HashMap<String, String> RusDictionary = loadDictinory("Rus");
+        HashMap<String, String> UaDictionary = loadDictinory("Ua");
+        HashMap<String, String> RusDictionaryTo = loadDictinoryTo("Rus");
+        HashMap<String, String> UaDictionaryTo = loadDictinoryTo("Ua");
         LIST.add(0, ENG);
         LIST.add(1, RUS);
         LIST.add(2, UA);
-        COUNT_OF_LANGUAGES.put("Rus", 0);
-        COUNT_OF_LANGUAGES.put("Eng", 0);
-        COUNT_OF_LANGUAGES.put("Ua", 0);
-
-    }
-
-    public static void setAListOfLanguages() {
         LIST_OF_LANGUAGES.put(0, "Eng");
         LIST_OF_LANGUAGES.put(1, "Rus");
         LIST_OF_LANGUAGES.put(2, "Ua");
+        COUNT_OF_LANGUAGES.put("Rus", 0);
+        COUNT_OF_LANGUAGES.put("Eng", 0);
+        COUNT_OF_LANGUAGES.put("Ua", 0);
+        DICTIONARY_LOAD.put("Rus", RusDictionary);
+        DICTIONARY_LOAD.put("Ua", UaDictionary);
+        DICTIONARY_LOAD_TO.put("Rus", RusDictionaryTo);
+        DICTIONARY_LOAD_TO.put("Ua", UaDictionaryTo);
+
     }
 
-    public static HashMap<String, Integer> findLanguageForTranslation(HashMap<Integer, String> map, int number,   //Не знаю,но думаю что я немного наблизился к тому что ты говорил(но это не точно)
+    public static HashMap<String, Integer> findLanguageForTranslation(HashMap<Integer, String> map, int number,
                                                                       HashMap<String, Integer> list) {
-        for(int x=0;x<LIST_OF_LANGUAGES.size();x++) {
+        for (int x = 0; x < LIST_OF_LANGUAGES.size(); x++) {
             if (map.get(number).contains(LIST_OF_LANGUAGES.get(x))) {
                 int count = list.get(LIST_OF_LANGUAGES.get(x));
                 list.put(LIST_OF_LANGUAGES.get(x), count + 1);
@@ -157,30 +179,40 @@ public class ModifTranslater {
         return list;
     }
 
-    public static void putNewLanguage() throws FileNotFoundException {
+    public static void putNewLanguage() {
         Scanner write = new Scanner(System.in);
         System.out.println("Enter new language");
         String language = write.nextLine();
+        HashMap<String, String> dictionaryNewLanguage = loadDictinory(language);
+        HashMap<String, String> dictionaryToNewLanguage = loadDictinoryTo(language);
         LIST_OF_LANGUAGES.put(LIST_OF_LANGUAGES.size(), language);
         COUNT_OF_LANGUAGES.put(language, 0);
-        final ArrayList<String> lang=listOfWords(language);
-        LIST.add(LIST.size(),lang);
+        final ArrayList<String> lang = listOfWords(language);
+        LIST.add(LIST.size(), lang);
+        DICTIONARY_LOAD.put(language, dictionaryNewLanguage);
+        DICTIONARY_LOAD_TO.put(language, dictionaryToNewLanguage);
 
     }
 
-    public static ArrayList<String> readFile(ArrayList<String> listOFword, String language) throws FileNotFoundException {
-        Scanner read = new Scanner(new FileReader(language));
-        if (language.equalsIgnoreCase("Eng")) {
-            while (read.hasNextLine()) {
-                String line = read.nextLine();
-                listOFword.add(line);
+    public static ArrayList<String> readFile(ArrayList<String> listOfWord, String language)  {
+        try {
+            Scanner read = new Scanner(new FileReader(language));
+            if (language.equalsIgnoreCase("Eng")) {
+                while (read.hasNextLine()) {
+                    String line = read.nextLine();
+                    listOfWord.add(line);
+                }
+            } else {
+                while (read.hasNextLine()) {
+                    String line = read.nextLine();
+                    String[] array = line.split("-");
+                    listOfWord.add(array[0]);
+                }
             }
-        } else
-            while (read.hasNextLine()) {
-                String line = read.nextLine();
-                String[] array = line.split("-");
-                listOFword.add(array[0]);
-            }
-        return listOFword;
+        }catch (FileNotFoundException error){
+            System.out.println("File("+language+") not found,please try again");
+            System.exit(-1);
+        }
+        return listOfWord;
     }
 }
