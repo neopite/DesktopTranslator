43cafc5336496f4897933d6b034d00eaf58ee8da
Yaroslav Obruch <Stamin4ik4@gmail.com>
2019-02-01 13:37:30 +0200

    refactor code

--- src/com/github/neophite/Translator/LanguageGuesser.java
+++ src/com/github/neophite/Translator/LanguageGuesser.java
@@ -1,25 +1,34 @@
 package com.github.neophite.Translator;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.*;
 
-import static com.github.neophite.Translator.TranslatorApp.LIST_OF_LANGUAGES;
-import static com.github.neophite.Translator.TranslatorApp.findSizeOfVocab;
-import static com.github.neophite.Translator.TranslatorApp.LIST;
-import static com.github.neophite.Translator.TranslatorApp.COUNT_OF_LANGUAGES;
 
 public class LanguageGuesser {
+    private static ArrayList<ArrayList<String>> LIST = new ArrayList<>();
+    private static HashMap<Integer, String> LIST_OF_LANGUAGES = new HashMap<>();
+    private static HashMap<String, Integer> COUNT_OF_LANGUAGES = new HashMap<>();
+
+    LanguageGuesser(ArrayList<ArrayList<String>> LIST, HashMap<Integer,
+            String> LIST_OF_LANGUAGE, HashMap<String, Integer> COUNT_OF_LANGUAGES) {
+        this.LIST = LIST;
+        this.LIST_OF_LANGUAGES = LIST_OF_LANGUAGE;
+        this.COUNT_OF_LANGUAGES = COUNT_OF_LANGUAGES;
+    }
+
     /**
      * Function returns language after checking all the words of the sentence for max similarity with this language.
      *
      * @param sentence -sentence from user.
      * @return language from list(Eng,Rus,Ua,etc).
      */
-    public static String languageDetection(String sentence) {
+    public String languageDetection(String sentence) {
         for (String word : sentence.split(" ")) {
             for (int x = 0; x < LIST.size(); x++) {
-                for (int z = 0; z < findSizeOfVocab(LIST_OF_LANGUAGES.get(x)); z++) {
+                int size = findSizeOfVocab(LIST_OF_LANGUAGES.get(x));
+                for (int z = 0; z < size; z++) {
                     if (word.equalsIgnoreCase((LIST.get(x).get(z)))) {
                         findLanguageForTranslation(LIST_OF_LANGUAGES, x, COUNT_OF_LANGUAGES);
                     }
@@ -45,8 +54,8 @@ public class LanguageGuesser {
      * @param list   - counter of each language in COUNT_OF_LANGUAGE.
      * @return update COUNT_OF_LANGUAGE.
      */
-    public static HashMap<String, Integer> findLanguageForTranslation(HashMap<Integer, String> map, int number,
-                                                                      HashMap<String, Integer> list) {
+    public HashMap<String, Integer> findLanguageForTranslation(HashMap<Integer, String> map, int number,
+                                                               HashMap<String, Integer> list) {
         for (int x = 0; x < LIST_OF_LANGUAGES.size(); x++) {
             if (map.get(number).contains(LIST_OF_LANGUAGES.get(x))) {
                 int count = list.get(LIST_OF_LANGUAGES.get(x));
@@ -56,4 +65,18 @@ public class LanguageGuesser {
         return list;
     }
 
+    public int findSizeOfVocab(String language) {
+        int count = 0;
+        File file = new File(language);
+        try (Scanner read = new Scanner(new FileReader(file))) {
+            while (read.hasNext()) {
+                String line = read.nextLine();
+                count++;
+            }
+        } catch (FileNotFoundException error) {
+            System.err.println("File(" + file + ") not found,please try again");
+            System.exit(-1);
+        }
+        return count;
+    }
 }
